b8fa31c2b8a5814a0bf5aec8c46826f3fbf2116d
HouariZEGAI <houarizegai14@gmail.com>
2018-12-27 11:38:58 +0100

    Update design & adding bar chart in Teacher Dashboard

--- src/com/databrains/bi4ss/java/controllers/TeacherDashboardController.java
+++ src/com/databrains/bi4ss/java/controllers/TeacherDashboardController.java
@@ -1,15 +1,15 @@
 package com.databrains.bi4ss.java.controllers;
 
 import com.jfoenix.controls.JFXComboBox;
+import javafx.beans.binding.Bindings;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.chart.LineChart;
-import javafx.scene.chart.PieChart;
-import javafx.scene.chart.XYChart;
+import javafx.scene.chart.*;
 
 import java.net.URL;
+import java.text.Format;
 import java.util.Random;
 import java.util.ResourceBundle;
 
@@ -22,7 +22,7 @@ public class TeacherDashboardController implements Initializable {
     // Data of Pie Chart
     private ObservableList<PieChart.Data> dataGenderChart;
     @FXML
-    private JFXComboBox comboYearGender;
+    private JFXComboBox<Integer> comboYearGender, comboYearModule;
 
     /* End Pie Chart */
 
@@ -35,24 +35,47 @@ public class TeacherDashboardController implements Initializable {
 
     /* End Line Chart */
 
+    /* Start Bar Chart */
+
+    @FXML
+    private BarChart barChart;
+
+    /* End Bar Chart */
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        initGenderChart();
         initCombo();
+        initGenderChart();
         initLineChart();
+        initBarChart();
     }
 
     private void initCombo() {
-        comboYearGender.getItems().addAll("2011", "2012", "2013", "2014", "2015", "2016");
+        for (int i = 2011; i < 2017; i++) {
+            comboYearGender.getItems().add(i);
+            comboYearModule.getItems().add(i);
+        }
         comboYearGender.getSelectionModel().select(0);
+        comboYearModule.getSelectionModel().select(0);
     }
 
     private void initGenderChart() {
+        int numOfMale = 60;
+        int numOfFamale = 70;
+
         dataGenderChart = FXCollections.observableArrayList();
-        dataGenderChart.add(new PieChart.Data("60 Male", 60d));
-        dataGenderChart.add(new PieChart.Data("70 Famale", 70d));
+        dataGenderChart.add(new PieChart.Data("Male", numOfMale));
+        dataGenderChart.add(new PieChart.Data("Famale", numOfFamale));
+        dataGenderChart.forEach(d
+                -> d.nameProperty().bind(
+                Bindings.concat((d.pieValueProperty() + "").replaceFirst(".*?(\\d+).*", "$1"), " ", d.getName())
+                )
+        );
+
         genderChart.setData(dataGenderChart);
     }
+    //
+    //
 
     private void initLineChart() {
         //defining a series
@@ -86,11 +109,44 @@ public class TeacherDashboardController implements Initializable {
             series2012.getData().set(i, new XYChart.Data(getMonthString(i + 1), i));
             series2013.getData().set(i, new XYChart.Data(getMonthString(i + 1), i * 2));
             series2014.getData().set(i, new XYChart.Data(getMonthString(i + 1), 12 - i));
-            series2015.getData().set(i, new XYChart.Data(getMonthString(i + 1), new Random().nextInt(10)));
+            series2015.getData().set(i, new XYChart.Data(getMonthString(i + 1), 2));
             series2016.getData().set(i, new XYChart.Data(getMonthString(i + 1), i - 1));
         }
 
         lineChart.getData().addAll(series2011, series2012, series2013, series2014, series2015, series2016);
+    }
+
+    private void initBarChart() {
+        CategoryAxis xAxis = (CategoryAxis) barChart.getXAxis();
+        NumberAxis yAxis = (NumberAxis) barChart.getYAxis();
+
+
+        XYChart.Series xyAlgo = new XYChart.Series();
+        XYChart.Series xyAnalyse = new XYChart.Series();
+        XYChart.Series xyAlgebra = new XYChart.Series();
+        XYChart.Series xySM = new XYChart.Series();
+
+        xyAlgo.setName("Algorithm");
+        xyAnalyse.setName("Analyse");
+        xyAlgebra.setName("Algebra");
+        xySM.setName("Structure Machine");
+
+        xyAlgo.getData().add(new XYChart.Data<>("September", 20));
+        xyAnalyse.getData().add(new XYChart.Data<>("September", 50));
+        xyAlgebra.getData().add(new XYChart.Data<>("September", 30));
+        xySM.getData().add(new XYChart.Data<>("September", 10));
+
+        xyAlgo.getData().add(new XYChart.Data<>("October", 20));
+        xyAnalyse.getData().add(new XYChart.Data<>("October", 10));
+        xyAlgebra.getData().add(new XYChart.Data<>("October", 80));
+        xySM.getData().add(new XYChart.Data<>("October", 30));
+
+        xyAlgo.getData().add(new XYChart.Data<>("November", 15));
+        xyAnalyse.getData().add(new XYChart.Data<>("November", 10));
+        xyAlgebra.getData().add(new XYChart.Data<>("November", 20));
+        xySM.getData().add(new XYChart.Data<>("November", 13));
+
+        barChart.getData().addAll(xyAlgo, xyAnalyse, xyAlgebra, xySM);
     }
 
     private String getMonthString(int month) {
