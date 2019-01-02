c695462f3edd7b575a2dcd6b7d9f2a135800390f
HouariZEGAI <houarizegai14@gmail.com>
2019-01-03 00:19:54 +0100

    Adding LMD Subjects

---
+++ src/com/databrains/bi4ss/java/type/Subjects.java
@@ -0,0 +1,7 @@
+package com.databrains.bi4ss.java.type;
+
+public class Subjects {
+    public final static String[] LMD1 = {"Algorithm", "Analyse", "Algebra", "Physical"};
+    public final static String[] LMD2 = {"ASD", "POO", "Architecture", "THL", "Logique Math"};
+    public final static String[] LMD3 = {"Compilation", "IHM", "Proba", "Proba"};
+}
