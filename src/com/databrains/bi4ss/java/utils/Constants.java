b67ecdd9ff64258f40db23d88067c26f6ba1abff
HouariZEGAI <houarizegai14@gmail.com>
2019-01-07 20:27:25 +0100

    Adding subjects & constants & params for send it to web service

---
+++ src/com/databrains/bi4ss/java/utils/Constants.java
@@ -0,0 +1,5 @@
+package com.databrains.bi4ss.java.utils;
+
+public class Constants {
+    public final static String[] LEVELS = {"MIAS1", "MIAS2", "MIAS3", "MGI1", "MGI2"};
+}
---
+++ src/com/databrains/bi4ss/java/utils/Params.java
@@ -0,0 +1,6 @@
+package com.databrains.bi4ss.java.utils;
+
+public class Params {
+    public static int selectedYear;
+    public static String selectedLevel;
+}
